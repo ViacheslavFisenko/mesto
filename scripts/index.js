87ea8757c0128b638a255c63cab29871095c8460
viacheslav <fisenkovyacheslav789@gmail.com>
2023-03-24 16:58:28 +0300

    finish vol.8

--- scripts/index.js
+++ scripts/index.js
@@ -3,7 +3,6 @@ const editPopup = document.querySelector(".popup");
 const editButtonClose = editPopup.querySelector(".popup__close");
 const nameImput = editPopup.querySelector(".popup__input_add_firstname");
 const dicsImput = editPopup.querySelector(".popup__input_add_disc");
-const editPopupSubmit = editPopup.querySelector(".popup__button");
 const editPopupForm = editPopup.querySelector(".popup__content")
 const profileName = document.querySelector(".profile__name")
 const discription = document.querySelector(".profile__description")
@@ -24,7 +23,6 @@ const closePopup = (editPopup) => {
   editPopup.classList.remove("popup_opened");
 }
 
-editButtonClose.addEventListener('click', closePopup(editPopup));
 
 editButtonClose.addEventListener("click", () => {
   closePopup(editPopup);
