61e3c7607c2f1557c115abb10ae105c2c164c312
viacheslav <fisenkovyacheslav789@gmail.com>
2023-04-09 14:54:10 +0300

    vol.9

--- scripts/index.js
+++ scripts/index.js
@@ -33,8 +33,8 @@ const addButton = document.querySelector(".profile__add");
 //далее идут переменные относящиеся к .popup
 const editPopup = document.querySelector(".popup");
 const editButtonClose = editPopup.querySelector(".popup-edit__close");
-const nameImput = editPopup.querySelector(".popup__input_add_firstname");
-const dicsImput = editPopup.querySelector(".popup__input_add_disc");
+const nameImput = editPopup.querySelector(".popup-edit__input_add_firstname");
+const dicsImput = editPopup.querySelector(".popup-edit__input_add_disc");
 const editPopupForm = editPopup.querySelector(".popup-edit__content")
 
 //далее идут переменные относящиеся к .popup_type_show-image/добавление карточек
