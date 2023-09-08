684a7c7bdccb232d45dc3aa2a2675a19d92eed89
ViacheslavFisenko <fisenkovyacheslav789@gmail.com>
2023-09-08 13:39:00 +0400

    start

---
+++ scripts/index.js
@@ -0,0 +1,192 @@
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg',
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+import { disableButton } from "./validate.js";
+
+const buttonEditProfile = document.querySelector(".profile__edit");
+const profileName = document.querySelector(".profile__name")
+const discription = document.querySelector(".profile__description")
+const buttonAddProfile = document.querySelector(".profile__add");
+
+//далее идут переменные относящиеся к .popup
+const editPopup = document.querySelector(".popup_type_show-edit");
+const buttonClosePopupProfile = editPopup.querySelector(".popup__close_type_edit");
+const nameImput = editPopup.querySelector(".popup__input_add_firstname");
+const dicsImput = editPopup.querySelector(".popup__input_add_disc");
+const editPopupForm = editPopup.querySelector(".popup__content_type_edit")
+
+//далее идут переменные относящиеся к .popup_type_show-image/добавление карточек
+const popupImg = document.querySelector(".popup_type_show-image");
+const popupImgButtonClose = document.querySelector(".popup__close_type_img");
+const popupImgForm = document.querySelector(".popup__content_type_img");
+const popupImgSubmitButton = popupImgForm.querySelector('.popup__submit-button')
+
+//далее идут переменные относящиеся к .popup-view/просмотр изображений
+const viewPopup = document.querySelector(".popup_type_show-view")
+const viewPopupButtonClose = document.querySelector(".popup__close_type_view")
+
+//эта переиспользуемая функция закрывает попапы
+const closePopup = (editPopup) => {
+  editPopup.classList.remove("popup_opened");
+  document.removeEventListener('keydown', handleCloseByEsc)
+}
+
+//эта переиспользуемая функция открывает попапы
+const openPopup = (editPopup) => {
+  editPopup.classList.add("popup_opened");
+  document.addEventListener('keydown', handleCloseByEsc)
+  disableButton('popup__submit-button_disabled', popupImgSubmitButton)
+}
+
+//ниже описан функционал редактирования профиля 
+buttonEditProfile.addEventListener("click", () => {
+  openPopup(editPopup);
+  nameImput.value = profileName.textContent;
+  dicsImput.value = discription.textContent;
+});
+
+buttonClosePopupProfile.addEventListener("click", () => {
+  closePopup(editPopup);
+});
+
+editPopupForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const name = nameImput.value;
+  profileName.textContent = name;
+  const disc = dicsImput.value;
+  discription.textContent = disc;
+  closePopup(editPopup);
+
+});
+
+//ниже описан рендеринг шаблона
+const cardsTemplate = document.getElementById("cards-template")
+const placesContainer = document.querySelector(".places__container")
+
+const createCardElement = (cardData) => {
+  const cardElement = cardsTemplate.content.querySelector(".place").cloneNode(true)
+  const cardTitle = cardElement.querySelector(".place__title")
+  const cardImage = cardElement.querySelector(".place__image")
+
+  const srcViewPopup = document.querySelector(".popup__image")
+  const popupViewDescription = document.querySelector(".popup__description")
+
+  cardTitle.textContent = cardData.name
+  cardImage.src = cardData.link
+  cardImage.alt = cardData.name
+
+  const deleteButton = cardElement.querySelector(".place__delete")
+  const likeButton = cardElement.querySelector(".place__like")
+
+  const handleDelete = () => {
+    cardElement.remove()
+  }
+
+  const handleLike = () => {
+    likeButton.classList.toggle('place__like-img_active')
+  }
+
+  deleteButton.addEventListener('click', handleDelete)
+
+  likeButton.addEventListener('click', handleLike)
+
+  cardImage.addEventListener('click', (event) => {
+    event.preventDefault();
+    openPopup(viewPopup);
+    srcViewPopup.src = cardData.link
+    srcViewPopup.alt = cardData.name
+    popupViewDescription.textContent = cardData.name
+  });
+
+  return cardElement
+}
+
+viewPopupButtonClose.addEventListener("click", () => {
+  closePopup(viewPopup);
+});
+
+const renderCardElement = (cardElement) => {
+  placesContainer.append(cardElement)
+}
+
+const renderNewCardElement = (cardElement) => {
+  placesContainer.prepend(cardElement)
+}
+
+initialCards.forEach((card) => {
+  renderCardElement(createCardElement(card))
+})
+
+//ниже описан функционал добавления карточек
+buttonAddProfile.addEventListener("click", () => {
+  openPopup(popupImg);
+  placeImput.value = ""
+  srcImput.value = ""
+});
+
+popupImgButtonClose.addEventListener("click", () => {
+  closePopup(popupImg);
+});
+
+const placeImput = popupImgForm.querySelector(".popup__input_add_place")
+const srcImput = popupImgForm.querySelector(".popup__input_add_src")
+
+const handleEditCardSubmit = (event) => {
+  event.preventDefault()
+  const name = placeImput.value
+  const link = srcImput.value
+  const placeData = {
+    name,
+    link,
+  }
+  renderNewCardElement(createCardElement(placeData))
+  closePopup(popupImg);
+}
+
+popupImgForm.addEventListener("submit", handleEditCardSubmit)
+
+// функционал закрытия на ESC
+
+const handleCloseByEsc = (event) => {
+  if (event.key === "Escape") {
+    const popupOpened = document.querySelector('.popup_opened')
+    closePopup(popupOpened)
+  }
+}
+
+const handleClosebyClickonOverlay = (event) => {
+  const popups = document.querySelectorAll('.popup')
+  const targetPopups = Array.from(popups)
+  targetPopups.forEach(function (popup) {
+    popup.addEventListener('click', (event) => {
+      if (event.target === event.currentTarget) {
+        closePopup(popup)
+      }
+    })
+  })
+}
+handleClosebyClickonOverlay()
---
+++ scripts/validate.js
@@ -0,0 +1,74 @@
+// валидация
+const setInputValidState = ({ inputErrorClass }, input, erorElement) => {
+  input.classList.remove(inputErrorClass)
+  erorElement.textContent = ' '
+}
+
+const setInputInvalidState = ({ inputErrorClass }, input, erorElement) => {
+  input.classList.add(inputErrorClass)
+  erorElement.textContent = input.validationMessage
+}
+
+const checkImputValidity = (rest, input) => {
+  const erorElement = document.querySelector(`#eror-${input.id}`)
+  //валидный
+  if (input.validity.valid) {
+    setInputValidState(rest, input, erorElement)
+    //невалидный
+  } else {
+    setInputInvalidState(rest, input, erorElement)
+  }
+}
+
+ export const disableButton = (inactiveButtonClass, button) => {
+  button.setAttribute('disabled', '')
+  button.classList.add(inactiveButtonClass)
+}
+
+const enableButton = (inactiveButtonClass, button) => {
+  button.removeAttribute('disabled')
+  button.classList.remove(inactiveButtonClass)
+}
+
+const toggleButtonValidity = ({ submitButtonSelector, inactiveButtonClass }, form) => {
+  const submitButton = form.querySelector(submitButtonSelector)
+  if (form.checkValidity()) {
+    enableButton(inactiveButtonClass, submitButton)
+  } else {
+    disableButton(inactiveButtonClass, submitButton)
+  }
+}
+
+const enableValidation = ({ formSelector, inputSelector, ...rest }) => {
+  const forms = document.querySelectorAll(formSelector) //присваиваю переменную всем формам в документе
+  const formsArray = Array.from(forms) //создал псевдо массив форм
+
+  //перебрал псевдомассив форм, нашел там кнопки отправки и отключил их настройки.
+  formsArray.forEach((form) => {
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+      toggleButtonValidity(rest, form)
+    })
+
+    toggleButtonValidity(rest, form)
+    
+    //перебрал псевдомассив импутов, нашел там все импуты и добавил валидацию на каждый импут.
+    const imputs = form.querySelectorAll(inputSelector) //присваиваю переменную всем импутам
+    const imputsArray = Array.from(imputs) //создал псевдо массив импутов
+    imputsArray.forEach((input) => {
+      input.addEventListener('input', () => {
+        checkImputValidity(rest, input)
+        toggleButtonValidity(rest, form)
+      })
+    })
+  })
+}
+
+enableValidation({
+  formSelector: '.popup__content',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__submit-button',
+  inactiveButtonClass: 'popup__submit-button_disabled',
+  inputErrorClass: 'popup__input_invalid',
+  errorClass: 'popup__eror-message'
+}); 
